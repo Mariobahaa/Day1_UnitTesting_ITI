83b52b5a79b5c89cc57558cc5f7014aca6485751
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2021-02-17 14:14:59 +0200

    Task1

---
+++ app/session_one/capitalizeText/__test__/capitalizeText.test.js
@@ -0,0 +1,16 @@
+import { capitalizeText } from "../index";
+
+describe('Capitalize text',() => {
+    test('should capitalize only the first letter',()=>{
+            const input = 'hamada';
+            const output = 'Hamada';
+            expect(capitalizeText(input)).toEqual(output);
+    });
+
+    test('should return an empty string',()=>{
+        const input = 12;
+        const output = '';
+        expect(capitalizeText(input)).toEqual(output);
+});
+
+});
