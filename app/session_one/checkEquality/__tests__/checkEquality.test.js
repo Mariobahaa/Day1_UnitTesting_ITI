85990e9571a6d0e07f14e64a6b2bd95cfad90b26
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2021-02-17 14:57:28 +0200

    Task 4

--- app/session_one/checkEquality/__tests__/checkEquality.test.js
+++ app/session_one/checkEquality/__tests__/checkEquality.test.js
@@ -1,9 +1,19 @@
-import { checkIfString } from "../index";
+import { checkIfEven, checkIfString } from "../index";
 
 describe("check equality", () => {
   test("should return true if got string", () => {
     expect(checkIfString("hamada")).toBeTruthy();
     expect(checkIfString(1)).toBeFalsy();
+  });
+
+  test("should return true if got string", () => {
+    expect(checkIfEven(2)).toBeTruthy();
+  
+  });
+
+  test("should return true if got string", () => {
+    expect(checkIfEven(3)).toBeFalsy();
+  
   });
 
   //   test("", () => {});
